7f2c1354262e38a23ffe42c9eb97fc50f33c0909
iyasmzn <iyasmzn07@gmail.com>
2022-04-05 05:20:14 +0000

    update

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-// This example uses the Google Maps JavaScript API's Data layer
-// to create a rectangular polygon with 2 holes in it.
+// Check for the various File API support.
+
+let map;
+
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 6,
-      center: { lat: -33.872, lng: 151.252 },
-    });
-    // Define the LatLng coordinates for the outer path.
-    const outerCoords = [
-      { lat: -32.364, lng: 153.207 },
-      { lat: -35.364, lng: 153.207 },
-      { lat: -35.364, lng: 158.207 },
-      { lat: -32.364, lng: 158.207 }, // north east
-    ];
-    // Define the LatLng coordinates for an inner path.
-    const innerCoords1 = [
-      { lat: -33.364, lng: 154.207 },
-      { lat: -34.364, lng: 154.207 },
-      { lat: -34.364, lng: 155.207 },
-      { lat: -33.364, lng: 155.207 },
-    ];
-    // Define the LatLng coordinates for another inner path.
-    const innerCoords2 = [
-      { lat: -33.364, lng: 156.207 },
-      { lat: -34.364, lng: 156.207 },
-      { lat: -34.364, lng: 157.207 },
-      { lat: -33.364, lng: 157.207 },
-    ];
-  
-    map.data.add({
-      geometry: new google.maps.Data.Polygon([
-        outerCoords,
-        innerCoords1,
-        innerCoords2,
-      ]),
-    });
-  }
+  let infoBox = document.getElementById('info-box')
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: { lat: -7, lng: 109 },
+  });
+  // NOTE: This uses cross-domain XHR, and may not work on older browsers.
+  map.data.loadGeoJson('./maps/multipolygon.geojson');
+  // map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/google.json');
+  // map.data.loadGeoJson('https://iniedu.com/geojson/maps.json');
+
+  // map.data.setStyle(function(feature) {
+  //   var TA = feature.getProperty('name')
+  //   var color = TA == 'ARJASARI' ? 'red' : 'violet'
+  //   const visible = TA == 'ARJASARI' ? true : false
+  //   return {
+  //     fillColor: color,
+  //     strokeWeight: 0,
+  //     fillOpacity: 1,
+  //     // visible: visible
+  //   };
+  // });
+
+  // map.data.addListener('click', function(event) {
+  //   map.data.overrideStyle(event.feature, {strokeWeight: 2, fillOpacity: 1});
+  //   infoBox.textContent = event.feature.getProperty('TA');
+  // });
+  // map.data.addListener('mouseover', function(event) {
+  //   map.data.overrideStyle(event.feature, {strokeWeight: 1, fillOpacity: .5});
+  // });
+
+  // map.data.addListener('mouseout', function(event) {
+  //   map.data.revertStyle();
+  // });
+}
